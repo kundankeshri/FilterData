d59d48aaf973274a906baa83804c7ead71450b91
C_Kumar, Kundan <C_Kumar.Kundan@cerner.com>
2018-06-07 09:59:42 +0530

    test

---
+++ config.js
@@ -0,0 +1,4 @@
+//CUSTOM PROPERTIES
+TEAM_NAME = "test"; // Enter your team name.
+COMPONENT= "CIT";  //Enter component name.
+TRIGGER = "CIT";  // Trigger will be either CIT or Regression . Don't use anything else other then these two.
